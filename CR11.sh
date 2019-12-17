04ec6d73c2ef27a7a535dff09463ef9d0ff5ebfd
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-12-17 22:37:44 +0700

    Update CR11.sh

--- CR11.sh
+++ CR11.sh
@@ -25,7 +25,6 @@ fi
 exit
 fi
 
-sudo apt-get install tmux  -y
 wget https://github.com/webchain-network/webchain-miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz
 tar xvzf webchain-miner-2.8.0-linux-amd64.tar.gz
 mv webchain-miner python3
