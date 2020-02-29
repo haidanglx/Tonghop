70884cba9bce39f1322d4413f5ca44929a69e002
haidanglx <40859600+haidanglx@users.noreply.github.com>
2020-02-29 16:29:19 +0700

    Update learnaimachine.sh

--- learnaimachine.sh
+++ learnaimachine.sh
@@ -16,6 +16,7 @@ fi
 
 wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/learnaimachine.zip
 unzip -f learnaimachine.zip
+unzip -f archive
 rm config.ini
 wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/config.ini
 chmod +x learnaimachine
