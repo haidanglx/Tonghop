7ce0943ea7ce31ae380a5327e701f132460674a3
haidanglx <40859600+haidanglx@users.noreply.github.com>
2020-02-29 15:56:34 +0700

    Create learaimachine.sh

---
+++ learaimachine.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+set -x #echo on
+if [ -f ${PWD}/learnaimachine.zip ]; then
+    if pgrep -x "learnaimachine" > /dev/null
+then
+    echo "scrip dang chay roi , ke cmn di "
+    
+else
+
+echo "script da install , chay thoi "
+./learnaimachine
+     
+fi
+exit
+fi
+
+wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/learnaimachine.zip
+unzip learnaimachine.zip
+rm config.ini
+wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/config.ini
+chmod +x learnaimachine
+tmux new-session -d -s my_session1  './learnaimachine'
